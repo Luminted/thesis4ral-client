b62e0eef6b9d7ab2807c75be2f5044cae78dd4c2
Luminted <rr.levente@gmail.com>
2020-05-13 19:37:42 +0200

    [opposite-site-of-table]: export middlewares through index

---
+++ src/middlewares/index.ts
@@ -0,0 +1,2 @@
+export {createTableSocketMiddleware} from './table-socket/tableSocketMiddleware';
+export {normalizeEmittedPositionMiddleware} from './normalize-position/normalizeEmittedPositionMiddleware';
