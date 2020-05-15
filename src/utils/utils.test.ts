eec7b64a3a232bc8312cc680450fbd24989d477d
Luminted <rr.levente@gmail.com>
2020-05-15 22:18:58 +0200

    [opposite-side-of-table]: adjusting test names

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,32 +1,32 @@
 import assert from 'assert';
-import {transformPositionToOrientation, inverseTransformPositionToOrientation} from './utils'
+import {mirrorOnTablePosition, inverseMirrorOnTablePosition} from './utils'
 
 describe('Testing utility functions', function(){
-    describe('transformPositionToOrientation', function(){
+    describe('mirrorOnTablePosition', function(){
         const positionX = 100;
         const positionY = 200;
         const tableWidth = 500;
         const tableHeight = 300;
 
         it('should mirror and translate by tables witdth on the x axis', function(){
-            const transformedPosition = transformPositionToOrientation(positionX, positionY, tableWidth, tableHeight);
+            const transformedPosition = mirrorOnTablePosition(positionX, positionY, tableWidth, tableHeight);
             assert.equal(transformedPosition[0], -positionX + tableWidth);
         })
         it('should mirror and translate by tables height on the y axis', function(){
-            const transformedPosition = transformPositionToOrientation(positionX, positionY, tableWidth, tableHeight);
+            const transformedPosition = mirrorOnTablePosition(positionX, positionY, tableWidth, tableHeight);
             assert.equal(transformedPosition[1], -positionY + tableHeight);
         })
     })
 
-    describe('inverseTransformPositionToOrientation', function(){
+    describe('inverseMirrorOnTablePosition', function(){
         const positionX = 100;
         const positionY = 200;
         const tableWidth = 500;
         const tableHeight = 300;
 
-        it('should invert transformPositionToOrientation', function(){
-            const transformedPosition = transformPositionToOrientation(positionX, positionY, tableWidth, tableHeight);
-            const invertedPosition = inverseTransformPositionToOrientation(transformedPosition[0], transformedPosition[1], tableWidth, tableHeight);
+        it('should invert mirrorOnTablePosition', function(){
+            const transformedPosition = mirrorOnTablePosition(positionX, positionY, tableWidth, tableHeight);
+            const invertedPosition = inverseMirrorOnTablePosition(transformedPosition[0], transformedPosition[1], tableWidth, tableHeight);
             assert.equal(invertedPosition[0], positionX);
             assert.equal(invertedPosition[1], positionY);
         })  
