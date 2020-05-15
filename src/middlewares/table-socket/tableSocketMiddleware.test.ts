73b1d8ba05b51c18310944843e7a13c5fec4a814
Luminted <rr.levente@gmail.com>
2020-05-15 22:14:20 +0200

    [opposite-side-of-table]: moving socket handling inside middleware

--- src/middlewares/table-socket/tableSocketMiddleware.test.ts
+++ src/middlewares/table-socket/tableSocketMiddleware.test.ts
@@ -2,106 +2,137 @@ import SocketIO from 'socket.io';
 import SocketIOClient from 'socket.io-client';
 import { createTableSocketMiddleware } from './tableSocketMiddleware';
 import { createMockMiddleware, MockMiddleware } from '../testutils';
-import { SocketActionTypeKeys, ActionTypes } from '../../actions/';
-import { TableSocketClientEvents } from './types';
+import { SocketActionTypeKeys, ActionTypes, SetActionTypeKeys, setGameState } from '../../actions/';
+import { TableSocketClientEvents, TableSocketServerEvents } from './types';
 import { Verb } from '../../types/verbTypes';
+import { SerializedGameState } from '../../types/dataModelDefinitions';
+import {} from 'redux-mock-store'
+import { SocketConnectionStatuses } from '../../types/additionalTypes';
+import { setTableSocketStatus } from '../../actions/setterActions';
 
 describe('Testing tableModuleMiddleware', function(){
-    const host = 'http://localhost';
-    const port = 4002;
-    const uri = `${host}:${port}`;
-    const namespace = '/dev'
-    const socketServer = SocketIO.listen(port);
-    const socketServerNamespace = socketServer.of(namespace);
-    const socket = SocketIOClient(`${uri}${namespace}`, {
-        autoConnect: false,
-        forceNew: true,
-    });
-    const tableSocketMiddleware = createTableSocketMiddleware(socket);
-    const mockMiddleware = createMockMiddleware<ActionTypes>(tableSocketMiddleware);
-
-    afterEach(() => {
-        socket.close();
-    })
-
-    afterAll((done) => {
-        socketServer.close(done);
-    })
-
-    it('should call next', function(){
-        const {invoke, next} = mockMiddleware;
-        invoke({
-            type: 'socket/'
-        } as unknown as ActionTypes); //forcing casting
-        expect(next).toHaveBeenCalled();
-    })
-    it(`should connect socket to given host and port on ${SocketActionTypeKeys.CONNECT}`, function(done){
-        const {invoke} = mockMiddleware;
-        const action: ActionTypes = {
-            type: SocketActionTypeKeys.CONNECT
-        }
-        socket.once('connect', () => {
-            expect(socket.connected).toBe(true);
-            done();
-        });
-        invoke(action);
-    })
-
-    it(`should disconnect on ${SocketActionTypeKeys.DISCONNECT}`, function(done){
-        const {invoke} = mockMiddleware;
-        const action: ActionTypes ={
-            type: SocketActionTypeKeys.DISCONNECT
-        }
-        socket.once('connect', () => {
-            invoke(action);
-            expect(socket.disconnected).toBe(true);
-            done();
-        })
-        socket.connect();
-    })
-
-    it(`should emit ${TableSocketClientEvents.VERB} event with verb and acknowledgement function of action on ${SocketActionTypeKeys.EMIT_VERB}`, function(done){
-        const emitSpy = jest.spyOn(socket, 'emit')
-        const {invoke} = mockMiddleware;
-        const action: ActionTypes = {
-            type: SocketActionTypeKeys.EMIT_VERB,
-            verb: {} as Verb,
-            ackFunction: jest.fn()
-        }
-
-        socket.once('connect', () => {
-            invoke(action);
-            expect(emitSpy).toHaveBeenCalledWith(TableSocketClientEvents.VERB, action.verb, action.ackFunction);
-            emitSpy.mockRestore();
-            done();
-        })
-        socket.connect();
-    })
-    it('should be able to perform side effects with acknowledgement function', function(done){
-        const {invoke} = mockMiddleware;
-        let testCounter = 0;
+    
+    
+    describe('Outgoing API', function(){
         
-        socketServerNamespace.once('connect', socket => {
-            socket.once(TableSocketClientEvents.VERB, (verb, ackFun) => {
-                ackFun();
+        const socketMock: SocketIOClient.Socket = {
+            connect: jest.fn(),
+            emit: jest.fn((event: string, ...data: any) => {}),
+            disconnect: jest.fn(),
+            on: jest.fn(),
+        } as unknown as SocketIOClient.Socket // forcing casting
+        const tableSocketMiddleware = createTableSocketMiddleware(socketMock);
+        const mockMiddleware = createMockMiddleware<ActionTypes>(tableSocketMiddleware);
+
+
+        describe(`Action: ${SocketActionTypeKeys.CONNECT}`, function(){
+            it(`should call sockets connect`, function(){
+                const {invoke} = mockMiddleware;
+                const action: ActionTypes = {
+                    type: SocketActionTypeKeys.CONNECT
+                }
+    
+                socketMock.connected = false;
+                invoke(action);
+                expect(socketMock.connect).toHaveBeenCalled();
+            })
+        })
+        describe(`Action: ${SocketActionTypeKeys.DISCONNECT}`, function(){
+            it(`should call sockets disconnect`, function(){
+                const {invoke} = mockMiddleware;
+                const action: ActionTypes ={
+                    type: SocketActionTypeKeys.DISCONNECT
+                }
+                invoke(action);
+                expect(socketMock.disconnect).toHaveBeenCalled();
+            })
+        })
+        describe(`Action: ${SocketActionTypeKeys.EMIT_VERB}`, function(){
+            it(`should emit ${TableSocketClientEvents.VERB} event with verb and acknowledgement function of action`, function(){
+                const {invoke} = mockMiddleware;
+                const action: ActionTypes = {
+                    type: SocketActionTypeKeys.EMIT_VERB,
+                    verb: {} as Verb,
+                    ackFunction: jest.fn()
+                }
+        
+                socketMock.connected = true;
+                invoke(action);
+                expect(socketMock.emit).toHaveBeenCalledWith(TableSocketClientEvents.VERB, action.verb, action.ackFunction);
             })
         })
 
-        socket.once('connect', () => {
-            const action: ActionTypes = {
-                type: SocketActionTypeKeys.EMIT_VERB,
-                verb: {} as Verb,
-                ackFunction: function() {
-                    testCounter++;
-                    // !!Assertion is done here!!
-                    expect(testCounter).toBe(1);
-                    done();
+        describe(`Action: ${SocketActionTypeKeys.JOIN_TABLE} with actions acknowledgement function`, function(){
+            it(`should emit ${TableSocketClientEvents.JOIN_TABLE}`, function(){
+                const {invoke} = mockMiddleware;
+                const action: ActionTypes = {
+                    type: SocketActionTypeKeys.JOIN_TABLE,
+                    ackFunction: jest.fn()
                 }
-            }
-            invoke(action);
+        
+                socketMock.connected = true;
+                invoke(action);
+                expect(socketMock.emit).toHaveBeenCalledWith(TableSocketClientEvents.JOIN_TABLE, action.ackFunction);
+            })
+        })
+    })
+
+    describe('Incoming API', function(){
+        const host = 'http://localhost';
+        const port = 4002;
+        const uri = `${host}:${port}`;
+        const namespace = '/dev'
+        const socketServer = SocketIO.listen(port);
+        const socketServerNamespace = socketServer.of(namespace);
+        const socket = SocketIOClient(`${uri}${namespace}`, {
+            autoConnect: false,
+            forceNew: true,
+        });
+        const tableSocketMiddleware = createTableSocketMiddleware(socket);
+        let mockMiddleware;
+        
+        beforeEach(done => {
+            mockMiddleware = createMockMiddleware<ActionTypes>(tableSocketMiddleware);
+            mockMiddleware.applyMiddleware();
+
+            socketServerNamespace.once('connect', () => {
+                console.log('client connected')
+            })
+            socket.once('connect', done);
+            socket.connect();
         })
 
-        socket.disconnect();
-        socket.connect();
+        afterEach(() =>{
+            socket.disconnect();
+        })
+
+        describe(`Event: ${TableSocketServerEvents.SYNC}`, function(){
+            it(`should dispatch ${SetActionTypeKeys.SET_GAME_STATE} with received gameState on ${TableSocketServerEvents.SYNC} event`, function(done){
+                const {store} = mockMiddleware;
+                const gameState = {
+                    cards: [],
+                    clients: [],
+                    decks: [],
+                    hands: []
+                } as SerializedGameState
+                socketServerNamespace.emit(TableSocketServerEvents.SYNC, gameState);
+                setTimeout(() => {
+                    expect(store.dispatch).toHaveBeenCalledWith(setGameState(gameState));
+                    done()
+                }, 100)
+            })
+        })
+
+        describe(`Event: ${TableSocketServerEvents.CONNECT}`, function(){
+            it(`should dispatch ${SetActionTypeKeys.SET_TABLE_CONNECTION_STATUS} action with status ${SocketConnectionStatuses.CONNECTED}`, function(done){
+                const {store} = mockMiddleware;
+                socket.disconnect();
+                socket.connect();
+                setTimeout(() => {
+                    expect(store.dispatch).toHaveBeenCalledWith(setTableSocketStatus(SocketConnectionStatuses.CONNECTED));
+                    done()
+                }, 100)
+            })
+        })
     })
 })
--- src/middlewares/table-socket/tableSocketMiddleware.ts
+++ src/middlewares/table-socket/tableSocketMiddleware.ts
@@ -4,20 +4,41 @@ import { TableSocketClientEvents, TableSocketServerEvents } from "./types";
 import { SocketActionTypeKeys } from '../../actions/socketActions';
 import { GameState } from '../../types/dataModelDefinitions';
 import { Middleware } from 'redux';
+import { setTableSocketStatus } from "../../actions/setterActions";
+import { SocketConnectionStatuses } from "../../types/additionalTypes";
 
 export function createTableSocketMiddleware (socket: SocketIOClient.Socket):  Middleware<{}, RootState>{
     return store => {
         const {dispatch} = store;
 
+        // Incoming API
         socket.on(TableSocketServerEvents.SYNC, (gameState: GameState) => {
-            dispatch(setGameState(gameState));
+            dispatch(setGameState(gameState))
+            
+        })
+
+        socket.on(TableSocketServerEvents.CONNECT, () => {
+            dispatch(setTableSocketStatus(SocketConnectionStatuses.CONNECTED))
+        })
+
+        socket.on('disconnect', () => {
+            //TODO: test this
+            dispatch(setTableSocketStatus(SocketConnectionStatuses.DISCONNECTED));
         })
 
 
         return next => 
-            (action: ActionTypes) => {
+        // Outgoing API
+        (action: ActionTypes) => {
                 if(action.type.startsWith('socket/')){
                     switch(action.type){
+                        case SocketActionTypeKeys.JOIN_TABLE:
+                            if(!socket.connected){
+                                console.log('emitting join: not connected')
+                            }else{
+                                socket.emit(TableSocketClientEvents.JOIN_TABLE, action.ackFunction);
+                            }
+                            break;
                         case SocketActionTypeKeys.EMIT_VERB:
                             if(!socket.connected){
                                 console.log('Middleware: Socket not connected!');
@@ -25,7 +46,7 @@ export function createTableSocketMiddleware (socket: SocketIOClient.Socket):  Mi
                             }
                             if(action.verb !== null){
                                 socket.emit(TableSocketClientEvents.VERB, action.verb, action.ackFunction);
-                                console.log(`Middleware: socket event emitted: type=${TableSocketClientEvents.VERB}, verb type=${action.verb.type}`  );
+                                console.log(`Middleware: socket event emitted: type=${TableSocketClientEvents.VERB}, verb type=${action.verb.type} position=${action.verb.positionX} ${action.verb.positionY}`  );
                             }else{
                                 console.log('Middleware: Verb to be emitted is NULL. Aborting emit.');
                                 return next(action);
@@ -33,7 +54,7 @@ export function createTableSocketMiddleware (socket: SocketIOClient.Socket):  Mi
                             break;
                         case SocketActionTypeKeys.CONNECT:
                             if(!socket.connected){
-                                socket.open();
+                                socket.connect();
                             }else{
                                 console.log('Middleware: Socket already connected');
                             }
