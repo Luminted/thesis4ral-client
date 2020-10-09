a8d43ac7ebd0e42ca409f761e40395bd36dbb25c
Levente Rozsenich <levente.rozsenich@benestudio.co>
2020-10-09 21:23:05 +0200

    adding TableApp to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import {Switch, Route, BrowserRouter} from "react-router-dom"
-
-import {TableApp} from './components/table-app/TableApp';
-import { useTypedSelector } from './store';
-import { selectTableConnectionStatus, selectOwnClientInfo } from './selectors';
-import { socketConnect, socketJoinTable, socketGetTableDimensions } from './actions/socketActions';
-import { SocketConnectionStatuses, Orientations } from './types/additionalTypes';
-import { mirrorVerbPositionMiddleware } from './middlewares/';
-import { readyTable } from './actions/thunks';
+import { TableApp } from "./views/TableApp";
 
 export function App() {
     
     return (
         <BrowserRouter>
             <Switch>
+                <Route exact={true} path={"/"} component={TableApp} />
             </Switch>
         </BrowserRouter>)
 }
