f190ab3a4b3088536d85c4145d0930e67e929a77
Levente Rozsenich <levente.rozsenich@benestudio.co>
2020-10-09 21:21:25 +0200

    creating ApplicationViewport for handling document wide events

---
+++ src/components/ApplicationViewport/ApplicationViewport.tsx
@@ -0,0 +1,56 @@
+import React, { useCallback, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import {throttle} from "lodash";
+import { selectGrabbedEntity } from "../../selectors";
+import { useTypedSelector } from "../../store";
+import {emitSharedVerb} from "../../actions/thunks";
+import { SharedVerbTypes } from "../../types/verbTypes";
+import "./style.css";
+import { CardTable } from "../CardTable/CardTable";
+
+const listenerThrottleValue = 1000 / 60;
+
+export const ApplicationViewport = () => {
+    const dispatch = useDispatch();
+
+    const grabbedEntity = useTypedSelector(selectGrabbedEntity);
+
+    const applicationViewportRef = useRef<HTMLDivElement>(null);
+
+    const onMouseMove = useCallback(throttle((event: MouseEvent) => {
+        if(grabbedEntity){
+            dispatch(emitSharedVerb(
+                event.clientX,
+                event.clientX,
+                SharedVerbTypes.MOVE,
+                grabbedEntity.entityId,
+                grabbedEntity.entityType));
+        }
+    }, listenerThrottleValue), [grabbedEntity]);
+
+    const onMouseUp = useCallback(throttle((event: MouseEvent) => {
+        if(grabbedEntity){
+            dispatch(emitSharedVerb(event.clientX,
+                event.clientY,
+                SharedVerbTypes.RELEASE,
+                grabbedEntity.entityId,
+                grabbedEntity.entityType));
+        }
+    }, listenerThrottleValue), [grabbedEntity]);
+
+    useEffect(() => {
+        applicationViewportRef.current?.addEventListener("mousemove", onMouseMove);
+        return () => applicationViewportRef.current?.removeEventListener("mousemove", onMouseMove);
+    }, [onMouseMove]);
+
+    useEffect(() => {
+        applicationViewportRef.current?.addEventListener("mouseup", onMouseUp);
+        return () => applicationViewportRef.current?.removeEventListener("mouseup", onMouseUp);
+    }, [onMouseUp]);
+
+    return (
+        <div ref={applicationViewportRef} className="application-viewport">
+            <CardTable/>
+        </div>
+    )
+}
---
+++ src/components/ApplicationViewport/index.ts
@@ -0,0 +1,1 @@
+export {ApplicationViewport} from "./ApplicationViewport";
