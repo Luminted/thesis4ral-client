326bc2583a6bcfb2a5d88a88bb4059cb31296c01
Levente Rozsenich <levente.rozsenich@benestudio.co>
2020-11-30 19:14:52 +0100

    moving socket initialization into middleware file

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,3 @@
-import SocketIOClient from 'socket.io-client';
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
@@ -20,19 +19,7 @@ const rootReducer = combineReducers({
 
 export type RootState = ReturnType<typeof rootReducer>;
 
-// TODO: clean this up
-const port = process.env.REACT_APP_SERVER_PORT;
-const host = process.env.REACT_APP_SERVER_HOST;
-const tableNamespace = '/table'
-const serverURL = `http://${host}:${port}${tableNamespace}`
-console.log(`connecting to ${serverURL}`)
-const socket = SocketIOClient(serverURL, {
-    // TODO: rethink this
-    query: {
-        tableId: 'dev'
-    }
-})
-const tableSocketMiddleware = createTableSocketMiddleware(socket);
+const tableSocketMiddleware = createTableSocketMiddleware();
 
 export const store = createStore(rootReducer, composeWithDevTools(
     // Order is important
