6d396229cda3541e0e4f9ae3342fde41b2a4c833
Levente Rozsenich <levente.rozsenich@benestudio.co>
2020-12-06 11:44:53 +0100

    removing emitPutOnTable

--- src/actions/thunks/emitCardVerb/index.ts
+++ src/actions/thunks/emitCardVerb/index.ts
@@ -2,5 +2,4 @@ export {emitAddCardVerb} from "./emitAddCardVerb"
 export {emitFlipVerb} from "./emitFlipVerb"
 export {emitGrabFromHand} from "./emitGrabFromHand"
 export {emitPutInHandVerb} from "./emitPutInHandVerb"
-export {emitPutOnTableVerb} from "./emitPutOnTableVerb"
 export {emitReorderHandVerb} from "./emitReorderHandVerb";
--- src/utils/isVerbWithPosition.ts
+++ src/utils/isVerbWithPosition.ts
@@ -1,9 +1,8 @@
-import { IAddCardVerb, IAddDeckVerb, IGrabFromHandVerb, IGrabVerb, IMoveToVerb, IMoveVerb, IPutOnTable, TVerb } from "../typings"
+import { IAddCardVerb, IAddDeckVerb, IGrabFromHandVerb, IGrabVerb, IMoveToVerb, IMoveVerb, TVerb } from "../typings"
 
 export const isVerbTypeWithPosition = (verb: TVerb): verb is IGrabVerb |
     IMoveToVerb |
     IMoveVerb |
     IAddCardVerb |
     IGrabFromHandVerb |
-    IPutOnTable |
     IAddDeckVerb => "positionX" in verb && "positionY" in verb; 
