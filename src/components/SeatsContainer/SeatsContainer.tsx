db8903ccad1d9834621cf7290de0b69056d2428d
Levente Rozsenich <levente.rozsenich@benestudio.co>
2020-12-12 21:29:32 +0100

    inverting seat oders if table is mirrored

--- src/components/SeatsContainer/SeatsContainer.tsx
+++ src/components/SeatsContainer/SeatsContainer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { selectClients } from "../../selectors";
+import { selectClients, selectIsMirrored } from "../../selectors";
 import { seatIdMapping } from "../../config";
 import { IProps } from "./typings";
 import { style } from "./style";
@@ -8,8 +8,10 @@ import { Seat } from "../Seat";
 
 export const SeatsContainer = ({ orientation }: IProps) => {
   const clients = useSelector(selectClients);
+  const isMirrored = useSelector(selectIsMirrored);
 
   const orientationsSeatIds = Object.keys(seatIdMapping).filter((id) =>  seatIdMapping[id]?.includes(orientation));
+  if(isMirrored) orientationsSeatIds.reverse();
 
   return (
     <>
