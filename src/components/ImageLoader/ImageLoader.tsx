70fa7ed052c5be5afca70c40c04b7145d6a6f661
Levente Rozsenich <levente.rozsenich@benestudio.co>
2020-12-15 01:39:27 +0100

    moving resources to local

--- src/components/ImageLoader/ImageLoader.tsx
+++ src/components/ImageLoader/ImageLoader.tsx
@@ -1,10 +1,29 @@
-import React from "react";
-import { serverHost, serverPort } from "../../config/connection";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import "./style.css";
 import { IProps } from "./typings";
 
 export const ImageLoader = ({ resourceRoute }: IProps) => {
-  const resourceURL = `http://${serverHost}:${serverPort}/${resourceRoute}.svg`;
+  const importedImageRef = useRef<any>(null);
+  const [loading, setLoading] = useState(false);
 
-  return <img alt="" className="svg-container" src={resourceURL} />;
+  useLayoutEffect(() => {
+    setLoading(true);
+    const importIcon = async () => {
+      try {
+        const { default: namedImport } = await import(`../../resources/${resourceRoute}.svg`);
+        importedImageRef.current = namedImport;
+        setLoading(false);
+      } catch (err) {
+        throw err;
+      } finally {
+        setLoading(false);
+      }
+    };
+    importIcon();
+  }, []);
+
+  return <>
+    {!loading && importedImageRef.current && <img alt="" className="svg-container" src={importedImageRef.current} />}
+  </>
+
 };
