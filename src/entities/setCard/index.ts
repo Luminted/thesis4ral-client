e71246a376b151f77d26ea7504d84020db75c933
Levente Rozsenich <levente.rozsenich@benestudio.co>
2020-12-17 22:02:45 +0100

    tweaking width for set cards

--- src/entities/setCard/index.ts
+++ src/entities/setCard/index.ts
@@ -3,6 +3,6 @@ import { ICardConfig } from "../../typings";
 export { setDeck } from "./setDeck";
 
 export const setCardConfig: ICardConfig = {
-  width: 300 * 5.5,
+  width: 370 * 5.5,
   height: 500 * 5.5,
 };
