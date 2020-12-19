d0d2ce37985b5b0a773dccc93b4022949dca11dd
Levente Rozsenich <levente.rozsenich@benestudio.co>
2020-12-20 00:01:26 +0100

    updating outgoing socket interface after server side change

--- src/actions/thunks/emitCardVerb/emitPutInHandVerb.ts
+++ src/actions/thunks/emitCardVerb/emitPutInHandVerb.ts
@@ -3,11 +3,13 @@ import { ECardVerbTypes, IPutInHandVerb } from "../../../typings";
 import { socketEmitVerb } from "../../";
 import { TSocketVerbAckFunction } from "../../socketEmitVerb";
 
-export const emitPutInHandVerb = (entityId: string, putInHandOf: string, faceUp: boolean, ackFunction?: TSocketVerbAckFunction): TThunkResult => (dispatch) => {
+export const emitPutInHandVerb = (entityId: string, toHandOf: string, faceUp: boolean, ackFunction?: TSocketVerbAckFunction): TThunkResult => (dispatch, getState) => {
+  const {clientInfo} = getState();
   const verb: IPutInHandVerb = {
     entityId,
     faceUp,
-    clientId: putInHandOf,
+    toHandOf: toHandOf,
+    clientId: clientInfo?.clientId || "",
     type: ECardVerbTypes.PUT_IN_HAND,
   };
   dispatch(socketEmitVerb(verb, ackFunction));
